f0b4546868f3d0d484ea037acd56fcf48731fcb4
Basti <basti@Bastis-MacBook-Air.local>
2018-01-13 12:46:20 +0100

    add some styles - footer style - list style - player style - playlist style - song style

---
+++ src/styles/Footer.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Footer = styled.nav`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background-color: rgba(255, 255, 255, .7);
+  border-top: 1px solid black;
+`;
